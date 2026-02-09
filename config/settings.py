8355a307694fc15a19c393a7154c57def8eb307c
Vusal Huseynov <vslh333222111@gmail.com>
2026-02-09 21:27:42 +0400

    Add CSRF_TRUSTED_ORIGINS to settings.py

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+from django.conf.global_settings import CSRF_TRUSTED_ORIGINS
 from pathlib import Path
 import environ
 import os
@@ -30,6 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', default=False)
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', default=[])
 
 
 # Application definition
